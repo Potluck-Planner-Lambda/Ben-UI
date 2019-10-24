9966546ca1e34cdfd215a42c5ddd661ebd077886
BenHall-7 <benjaminjahall@gmail.com>
2019-10-24 02:19:51 -0400

    use github API for team information on About

--- js/about.js
+++ js/about.js
@@ -1,9 +1,53 @@
+const teamGitHubHandles = [
+    'BenHall-7',
+    'calebredd',
+    'cnlien',
+    'elijahdaniel',
+    'LeTanque',
+    'Tereamarie'
+];
 
+function element(name) {
+    return document.createElement(name);
+}
+
+function textNode(data) {
+    return document.createTextNode(data);
+}
+
+function addUserData(user) {
+    let listItem = element("li");
+
+    let imgNode = element("img");
+    imgNode.setAttribute("src", user.avatar_url);
+    
+    let name = element("p");
+    let name_text = textNode(user.name);
+    name.appendChild(name_text);
+
+    let anchor = element("a");
+    anchor.setAttribute("href", user.html_url);
+    let anchor_text = textNode("Profile");
+    anchor.appendChild(anchor_text);
+
+    listItem.appendChild(imgNode);
+    listItem.appendChild(name);
+    listItem.appendChild(anchor);
+    
+    document.getElementById("members").appendChild(listItem);
+}
 
 function load() {
-    document.getElementsByClassName("about")[0].appendChild(
-        document.createElement("p").appendChild(
-            document.createTextNode("test")
-        )
-    );
+    teamGitHubHandles.forEach(async (handle) => {
+        try {
+            let response = await fetch(`https://api.github.com/users/${handle}`, {
+                credentials: "omit"
+            });
+            let json = await response.json();
+            addUserData(json);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    })
 }
